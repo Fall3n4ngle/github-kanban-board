3db3e33cb7562fc68f3f46b0fa7280c1fe3c0b2c
Alex <sashaoliyar7@gmail.com>
2024-04-08 21:23:55 +0300

    configured redux-toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./app/store.ts";
 
-import './index.css'
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ReduxProvider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ReduxProvider>
   </React.StrictMode>,
 );
