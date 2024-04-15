0de498d22ffb1792a33aeeca412e2c538949fb20
Alex <sashaoliyar7@gmail.com>
2024-04-15 13:17:08 +0300

    removed createSelector on selectIssuesByKey

--- src/features/issues/store/selectors.ts
+++ src/features/issues/store/selectors.ts
@@ -1,10 +1,6 @@
-import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "@/app/store";
 
 export const selectIssuesStatus = (state: RootState) => state.issues.isLoading;
 export const selectIssuesError = (state: RootState) => state.issues.error;
-export const selectIssuesByKey = createSelector(
-  (state: RootState) => state.issues.data,
-  (_: RootState, issuesKey: string) => issuesKey,
-  (issues, issuesKey) => issues[issuesKey]
-);
+export const selectIssuesByKey = (state: RootState, issuesKey: string) =>
+  state.issues.data[issuesKey];
